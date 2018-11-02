28642d244adff92bc94410b6e3dd7337483be4d5
JanhonChen <lantize@gmail.com>
2018-11-03 00:48:31 +0800

    更新Auto Layout

--- LetMuGO/ViewController.swift
+++ LetMuGO/ViewController.swift
@@ -14,6 +14,7 @@ import GoogleMobileAds
 class ViewController: UIViewController {
     @IBOutlet weak var indexView: UIImageView!
     @IBOutlet weak var bannerView: GADBannerView!
+    @IBOutlet var imageView: UIImageView!
     
     
     var audioPlayer : AVAudioPlayer!
@@ -23,15 +24,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         let str = NSLocalizedString("CFBundleDisplayName", comment: "title")
         print(str)
-        
         let request = GADRequest()
         bannerView.adUnitID = "ca-app-pub-5799388280198989/9699056235"
         bannerView.rootViewController = self
         bannerView.load(request)
         
         request.testDevices = [ kGADSimulatorID ]
-    
+        
+
     }
+    
     
     @IBAction func startPressed(_ sender: UIButton) {
         playSound(soundFileName: "guitar")
@@ -49,6 +51,8 @@ class ViewController: UIViewController {
         }
         audioPlayer.play()
     }
+    
+    
 
 }
 
